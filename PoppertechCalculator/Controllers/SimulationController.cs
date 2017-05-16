8d2009eb947f24657a579107e3fb089a7154191b
poppertech <bwynne@poppertech.com>
2017-05-16 18:50:10 -0400

    Added the simulation controller

---
+++ PoppertechCalculator/Controllers/SimulationController.cs
@@ -0,0 +1,37 @@
+﻿using PoppertechCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace PoppertechCalculator.Controllers
+{
+    [RoutePrefix("simulation")]
+    public class SimulationController : ApiController
+    {
+        public SimulationController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("")]
+        [ResponseType(typeof(string))]
+        public IHttpActionResult Get()
+        {
+            return Ok("Success");
+        }
+
+
+        [HttpPost]
+        [Route("")]
+        [ResponseType(typeof(string))]
+        public IHttpActionResult Post([FromBody] ScenarioRequest request)
+        {
+            return Ok(request.Test);
+        } 
+    }
+}
