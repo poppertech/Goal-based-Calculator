bf8fe3ff151986a647037374ebb3229700e08c16
poppertech <bwynne@poppertech.com>
2017-07-08 16:33:42 -0400

    Added forecast to data model

---
+++ PoppertechCalculator/Models/Forecast.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PoppertechCalculator.Models
+{
+    public class Forecast
+    {
+        public decimal Minimum { get; set; }
+        public decimal Worst { get; set; }
+        public decimal Likely { get; set; }
+        public decimal Best { get; set; }
+        public decimal Maximum { get; set; }
+    }
+}
