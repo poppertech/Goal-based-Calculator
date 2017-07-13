9c5286cd86f22dc7119b95f1f01147f7bdbb5752
poppertech <bwynne@poppertech.com>
2017-07-13 07:51:57 -0400

    Included monte carlo results

---
+++ PoppertechCalculator/Models/MonteCarloResults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PoppertechCalculator.Models
+{
+    public class MonteCarloResults
+    {
+        public int[] AreaNumbers { get; set; }
+        public decimal[] Simulations { get; set; }
+    }
+}
