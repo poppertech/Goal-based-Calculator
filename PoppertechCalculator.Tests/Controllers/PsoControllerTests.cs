ad52b4fc08884e3866050f6ac8932ea52d4891d5
poppertech <bwynne@poppertech.com>
2017-08-25 06:18:46 -0400

    Added Unit Tests File

---
+++ PoppertechCalculator.Tests/Controllers/PsoControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PoppertechCalculator.Models.PSO;
+using Moq;
+using PoppertechCalculator.Logic.Interfaces.Pso;
+using PoppertechCalculator.Controllers;
+using PoppertechCalculator.Models;
+using System.Web.Http.Results;
+
+namespace PoppertechCalculator.Tests.Controllers
+{
+    [TestClass]
+    public class PsoControllerTests
+    {
+        [TestMethod]
+        public void PostOnSuccessReturnsResponse()
+        {
+            //arrange
+            var psoResults = new PsoResults();
+            var psoContext = new PsoContext();
+
+            var processor = new Mock<IPsoCalculationsProcessor>();
+            processor.Setup(p => p.OptimizePortfolio(It.IsAny<PsoContext>())).Returns(psoResults);
+
+            var controller = new PsoController(processor.Object);
+
+            //act
+            var result = controller.Post(psoContext) as OkNegotiatedContentResult<Response<PsoResults>>;
+
+            //assert
+            Assert.AreSame(psoResults, result.Content.Model);
+        }
+    }
+}
